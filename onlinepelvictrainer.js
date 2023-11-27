f60ff611a3e5c663b91ead9dfcb4a2054928ae1f
jhtcodes <147118679+jhtcodes@users.noreply.github.com>
2023-11-27 12:01:16 +1100

    Add files via upload

--- onlinepelvictrainer.js
+++ onlinepelvictrainer.js
@@ -1,28 +1,51 @@
+// steps:
+// 1. click start button - done
+// 2. get squeezeTimer to start counting from the number of seconds written in squeeze input
+// 3. get relaxTimer to start counting from teh umber of seconds wrriten in relax input
+// 4. get squeeze timer and relax timer to continue to repeat for the number of reps written in repetition time
+// 5. end with a prompt that says "Good work on completing your pelvic floor exercises! See you for the next round!"
+
+// other things taht can be done - button for set time squeeze/ relax/ rept time
 
 
-let timeLeft1 = 10;
-let timeLeft2 = 9;
+const startBtn = document.querySelector('.start-button');
+const squeezeTime = document.querySelector('#squeeze-number')
+const relaxTime = document.querySelector('#relax-number')
+const repetitions = document.querySelector('#rep-number')
 
-function countdown1() {
-	if (timeLeft1 == 0) {
-		clearTimeout(timerId1);
-		console.log("Countdown finished!");
-		const timerId2 = setInterval(coountdown2, 1000);
-	} else {
-		console.log(timeLeft + " seconds remaining");
-		timeLeft1--;
+
+startBtn.addEventListener('click', function() {
+	console.log('start button working')
+	squeezeTimer();
+})
+
+function squeezeTimer () {
+	let timeLeft = squeezeTime.value;
+
+	function countdown() {
+		if (timeLeft == 0) {
+			clearTimeout(timerId);
+			console.log("Squeeze Timer finished!");
+			relaxTimer ()
+		} else {
+			console.log(timeLeft + " seconds remaining");
+			timeLeft--;
+		}
 	}
-}
+	
+	let timerId = setInterval(countdown, 1000);}
 
-function countdown2() {
-	if (timeLeft2 === 0) {
-		clearTimeout(timerId2);
-		console.log("Countdown 2 Finished!");
-	} else {
-		console.log("Countdown 2: "+ timeLeft2 + " seconds remaining");
-		timeLeft2--;
+function relaxTimer () {
+	let timeLeft = relaxTime.value;
+
+	function countdown() {
+		if (timeLeft == 0) {
+			clearTimeout(timerId);
+			console.log("Relax Timer finished!");
+		} else {
+			console.log(timeLeft + " seconds remaining");
+			timeLeft--;
+		}
 	}
-}
-
-const timerId1 = setInterval(countdown, 1000);
-const timerId2 = setInterval(countdown, 1000);
+	
+	let timerId = setInterval(countdown, 1000);}
