2782ae6a7d34bbf3dbd5c62d450a2967c6f24033
jhtcodes <147118679+jhtcodes@users.noreply.github.com>
2023-12-02 20:53:47 +1100

    Add files via upload

--- onlinepelvictrainer.js
+++ onlinepelvictrainer.js
@@ -12,40 +12,85 @@ const startBtn = document.querySelector('.start-button');
 const squeezeTime = document.querySelector('#squeeze-number')
 const relaxTime = document.querySelector('#relax-number')
 const repetitions = document.querySelector('#rep-number')
+const squeezeBtn = document.querySelector('.squeeze-btn')
+const relaxBtn = document.querySelector('.relax-btn')
+const repetitionBtn = document.querySelector('.repetitions-btn')
+const onRepeat = document.querySelector('#rep-number')
+const transitionBar = document.querySelector('.timer-bar')
+const transitionBarProgress = document.querySelector('.timer-bar-progress')
+const timeSpan = document.querySelector('.time')
 
-
-startBtn.addEventListener('click', function() {
-	console.log('start button working')
-	squeezeTimer();
+startBtn.addEventListener("click", function () {
+	console.log("start button working")
+	RepeatTheSet ()
 })
 
-function squeezeTimer () {
-	let timeLeft = squeezeTime.value;
-
-	function countdown() {
-		if (timeLeft == 0) {
-			clearTimeout(timerId);
-			console.log("Squeeze Timer finished!");
-			relaxTimer ()
-		} else {
-			console.log(timeLeft + " seconds remaining");
-			timeLeft--;
-		}
-	}
+function RepeatTheSet () {
+	let RepeatCounter = onRepeat.value;
+	RepeatCounter--;
 	
-	let timerId = setInterval(countdown, 1000);}
-
-function relaxTimer () {
-	let timeLeft = relaxTime.value;
-
-	function countdown() {
-		if (timeLeft == 0) {
-			clearTimeout(timerId);
-			console.log("Relax Timer finished!");
-		} else {
-			console.log(timeLeft + " seconds remaining");
-			timeLeft--;
-		}
+	if (RepeatCounter =>1) {
+		numbersDown ();
+	} else {
+		alert("completed task today")
 	}
-	
-	let timerId = setInterval(countdown, 1000);}
+}
+
+function numbersDown () {
+	let intervalFirst = squeezeTime.value;
+	const countDown = setInterval(() => {
+		intervalFirst --;
+		let transitionBarWidth = intervalFirst / squeezeTime.value * 100
+
+		if (intervalFirst == 0) {
+			clearInterval(countDown)
+			transitionBar.style.width = "0%";
+			timeSpan.innerHTML = "0s"
+			numbersUp ()
+		} else {if (intervalFirst >= 1) {
+			transitionBar.style.width = transitionBarWidth + "%"
+			timeSpan.innerHTML = intervalFirst + 's'
+		}} 
+}, 1000)}
+;
+
+function numbersUp () {
+	let intervalSecond = relaxTime.value;
+	let count = 0;
+	const countUp = setInterval(() => {
+		count++;
+		let transitionBarWidth = count / relaxTime.value * 100
+		if (count < intervalSecond) {
+			transitionBar.style.width = transitionBarWidth + "%"
+			timeSpan.innerHTML = intervalSecond + 's'
+		} else if (count >= relaxTime.value) { 
+			clearInterval(countUp)
+			transitionBar.style.width = "100%";
+			timeSpan.innerHTML = relaxTime.value + 's'
+		} 
+}, 1000);
+}
+
+
+squeezeBtn.addEventListener("click", addSqueezeTimeToSqueeze())
+function addSqueezeTimeToSqueeze() {
+	console.log('squeeze btn working')
+}
+relaxBtn.addEventListener("click", function () {
+	console.log("relax button working")
+})
+
+
+repetitionBtn.addEventListener("click", function () {
+	console.log("rep button working")
+})
+
+
+// function repetitionTimer () {
+// 	if (currentRepetition >= repetitions) {
+// 		return;
+// 	}
+// 	setTimeouut(() => {
+// 		transitionBar.computedStyleMap.
+// 	})
+// }
